221e562f7d272f41eb0a344cdb6f3a6022769ead
LARRY SWAN <G00386837@gmit.ie>
2022-04-19 16:22:37 +0100

    map and qr scanner

--- PPIT Project/src/components/pages/GPS.js
+++ PPIT Project/src/components/pages/GPS.js
@@ -1,13 +1,27 @@
 import React from "react";
-import '../../App.css';
+import ReactDOM from 'react-dom';
+import {Map,MapContainer,Marker,Popup,TileLayer} from "react-leaflet";
+import{Icon} from "leaflet";
+import * as parkData from "./Gaa-parks.json"; 
+import axios from 'axios';
+import "./GPs.css";
+
 
 export default function GPS(){
-    return(
-        <ul className='GPS'>
-             <div>
-                <h1>GPS</h1>
 
-            </div>             
-        </ul>
-    );
+    return<MapContainer center={[53.270668, -9.056790]} zoom ={12}>
+        <TileLayer 
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>
+        contributors'
+        />
+        {parkData.pitches.map(park=>(<Marker key={park.name} position={[park.position[0],park.position[1]
+        ]}
+        onClick={() =>{
+            
+        }}
+        ></Marker>))}
+    </MapContainer>;
+        
+    
 }
--- PPIT Project/src/components/pages/QR.js
+++ PPIT Project/src/components/pages/QR.js
@@ -1,12 +1,44 @@
-import React from "react";
-import '../../App.css';
 
-export default function QR(){
-    return(
-        <ul className='QR'>
-               <div>
-                   <h1>QR</h1>
-               </div>              
-        </ul>
-    );
-}
+import '../../App.css';
+import React, { Component } from 'react'
+import QrReader from 'react-qr-scanner'
+ 
+          class Qr extends Component {
+          constructor(props){
+            super(props)
+            this.state = {
+              delay: 100,
+              result: 'No result',
+            }
+        
+            this.handleScan = this.handleScan.bind(this)
+          }
+          handleScan(data){
+            this.setState({
+              result: data,
+            })
+          }
+          handleError(err){
+            console.error(err)
+          }
+          render(){
+            const previewStyle = {
+              height: 240,
+              width: 320,
+            }
+        
+            return(
+              <div>
+                <QrReader
+                  delay={this.state.delay}
+                  style={previewStyle}
+                  onError={this.handleError}
+                  onScan={this.handleScan}
+                  />
+                <p>{this.state.result}</p>
+              </div>
+            )
+          }
+        }
+        
+export default Qr;
